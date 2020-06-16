cdb6e3a35cc8da12334419ff1ad3fcded6c22364
unknown <wxj@DAD00001872.Infinitus.com.cn>
2020-06-16 17:35:56 +0800

    二叉树

---
+++ pages/高级/二叉树/binaryTree.js
@@ -0,0 +1,22 @@
+class TreeNode {
+  constructor(val){
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+class BinaryTree {
+  constructor () {
+    this.root = null
+  }
+  insert (val) {
+    
+  }
+  toString () {
+
+  }
+}
+export {
+  TreeNode,
+  BinaryTree
+}
